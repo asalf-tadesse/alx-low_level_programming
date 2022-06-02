d3c6beab31330b80a9bd825eadfc96d9de02ee8d
asalf-tadesse <asawtadesse@gmail.com>
2022-06-02 13:06:58 -0700

    git correction

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -8,13 +8,8 @@
  *   * Return: 0
  *
  */
-
 int main(void)
-
 {
-
-	puts("\"Programming is like building a multilingual puzzle\n");
-
+	puts("\"Programming is like building a multilingual puzzle");
 	return (0);
-
 }
