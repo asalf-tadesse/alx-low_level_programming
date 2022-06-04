8084328e2e293ac00d82a0715cd73c03ff081698
asalf-tadesse <asawtadesse@gmail.com>
2022-06-03 19:56:58 -0700

    task3-9

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main (void)
+{
+	char c = 'a';
+
+	while()
+	{
+		putchat(c);
+		c++;
+	
+	}
+	putchat('\n');
+	return (0);
+
+}
---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,28 @@
+#include <stdlib.h>
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	char c = 'a';
+		
+	while(c <= 'z')
+	{
+		putchar(c);
+		c++;
+	
+	}
+	c='A';
+	while()
+	{
+		putchar(c);
+		c++;
+	
+	}
+	putchar('\n');
+	return (0);
+
+}
