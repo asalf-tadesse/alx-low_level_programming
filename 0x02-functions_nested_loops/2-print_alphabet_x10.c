fcb71ac24caeff64180ba0a8e0664e70fad39a0e
asalf-tadesse <asawtadesse@gmail.com>
2022-06-07 11:17:51 -0700

    task2

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -9,14 +9,14 @@ void print_alphabet_x10(void)
 {
 	int i;
 
-	for( i = 0; i < 10; i++)
+	for (i = 0; i < 10; i++)
 	{
 		char c;
 
-		for( c = 'a'; c <= 'z'; c++)
+		for (c = 'a'; c <= 'z'; c++)
 		{
 			_putchar(c);
 		}
 		_putchar('\n');
-	}	
+	}
 }
