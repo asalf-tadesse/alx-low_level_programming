d02a00655a51b1d6e3c8dc43a1b2a12890131ce8
asalf-tadesse <asawtadesse@gmail.com>
2022-06-07 10:18:06 -0700

    nested

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+/*
+ * main - Print the word _putchar
+ *
+ * return: 0 Sucess
+ *
+ */
+int main(void)
+{
+	char put[8] = "_putchar";
+
+	for(int i=0; i<=7; i++)
+	{
+		putchar(put[i]);
+	}
+	putchar('\n');
+	return (0);
+}
---
+++ 0x02-functions_nested_loops/README.md
@@ -0,0 +1,1 @@
+function neste loop
