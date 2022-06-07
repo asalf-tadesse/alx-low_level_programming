21e0c71f73b6bb551a8007846013f0179b229951
asalf-tadesse <asawtadesse@gmail.com>
2022-06-07 13:00:18 -0700

    t-4

---
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -0,0 +1,20 @@
+#include "main.h"
+#include <ctype.h>
+/**
+ * _isalpha - Function checks is a capital letter
+ *
+ * @c: character to be checked
+ *
+ * Return: 1 if c is letter 0 otherwise
+ */
+int _isalpha(int c)
+{
+	if (isalpha(c))
+	{
+		return (1);
+	}
+	else 
+	{
+		return (0);
+	}
+}
