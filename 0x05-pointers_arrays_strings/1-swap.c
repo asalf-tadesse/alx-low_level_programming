01e438e21224e429cd842e806eb28c8c40c724be
asalf-tadesse <asawtadesse@gmail.com>
2022-06-13 05:09:36 -0700

    task 1

--- 0x05-pointers_arrays_strings/1-swap.c
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -2,18 +2,19 @@
 #include <stdio.h>
 
 /**
- * function: to swap_int two numbers 
+ * function: to swap_int two numbers
  *
- * declar temporary variable 
- * 
+ * declar temporary variable
+ *
  * and stor a on tmp then assign b to a then assign tmp to b
- * 
+ *
  * return the swaped valu
  */
 void swap_int(int *a, int *b)
 {
-	int *tmp;
-	*tmp = *a;
+	int tmp;
+
+	tmp = *a;
 	*a = *b;
 	*b = *tmp;
 }
