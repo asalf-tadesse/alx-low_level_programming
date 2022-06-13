e322c35f8fa01568e4d95b0b2a2110e730a82caf
asalf-tadesse <asawtadesse@gmail.com>
2022-06-13 13:35:02 -0700

    last correction

--- 0x05-pointers_arrays_strings/7-puts_half.c
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -14,15 +14,15 @@ void puts_half(char *str)
 	len = strlen(str);
 	if (len % 2 == 0)
 	{
-		for (i = len / 2; i < len; i ++)
+		for (i = len / 2; i < len; i++)
 		{
 			_putchar(str[i]);
 		}
 		_putchar('\n');
 	}
-	else if(len  % 2 == 1)
+	else if (len  % 2 == 1)
 	{
-		for (i = ((len + 1) / 2); i < len; i ++)
+		for (i = ((len + 1) / 2); i < len; i++)
 		{
 			_putchar(str[i]);
 		}
